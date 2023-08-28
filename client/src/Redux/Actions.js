6fcdafb1f4e899b4a8a05d6371d69d843940cbe3
Hawkins5 <hmasd05@gmail.com>
2023-08-28 17:43:46 -0500

    .

---
+++ client/src/Redux/Actions.js
@@ -0,0 +1,10 @@
+export const loadCards = (cards) => ({
+    type: 'LOAD_CARDS',
+    payload: cards,
+  });
+  
+
+  export const applyFilter = (filter) => ({
+    type: 'APPLY_FILTER',
+    payload: filter,
+  });
