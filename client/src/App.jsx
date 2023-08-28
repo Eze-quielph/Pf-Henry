7523763f7130c576e520b7daa49cf90571e3158c
Wpxchi <urielcastillo891@gmail.com>
2023-08-28 12:36:16 -0300

    configuracion socketIo en front y back

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
-
+import { Route, Routes } from 'react-router-dom'
+import ChatenVivo from './SocketIo/Chat'
 import './App.css'
 
 function App() {
  
   return (
     <div>
-    <h1>Spoty Fans</h1>
+      <h1>Spoty Fans</h1>
+      <Routes>
+        <Route path='/chat' element={<ChatenVivo/>}/>
+      </Routes>
      </div>
   )
 }
