641ecdaf22066c99216de6de43239ba90cd40c5b
Andres Torres <andres.torressandoval@hotmail.com>
2023-08-29 11:05:14 -0500

    Andres/ pag terminos con configuracion de tailwind

--- client/src/Pages/Terns/TermsData/10.ContenidoPriohibido.js
+++ client/src/Pages/Terns/TermsData/10.ContenidoPriohibido.js
@@ -4,6 +4,6 @@ export const ContenidoProhibido = {
 
     description1: "No debe cargar ni compartir Contenido del usuario que, de conformidad con el criterio razonable de Spootichat, tenga estas características:",
 
-    description2: "cconstituye o promociona una copia ilegal o no autorizada de la obra protegida con derechos de autor de otra persona, como brindar programas de computadora pirateados o vínculos hacia estos, brindar información para evadir dispositivos con protección de copia instalada por el fabricante o brindar música pirateada o vínculos hacia archivos de música pirateada."
+    description2: "Constituye o promociona una copia ilegal o no autorizada de la obra protegida con derechos de autor de otra persona, como brindar programas de computadora pirateados o vínculos hacia estos, brindar información para evadir dispositivos con protección de copia instalada por el fabricante o brindar música pirateada o vínculos hacia archivos de música pirateada."
 
 }
--- client/src/Pages/Terns/TermsData/13.Privacidad.js
+++ client/src/Pages/Terns/TermsData/13.Privacidad.js
@@ -4,6 +4,6 @@ export const Privacidad = {
 
     description1: "La Política de privacidad de Spootichat forma parte de este Acuerdo y se incorpora en este documento como referencia.",
 
-    description2: "Si tiene alguna pregunta, reclamo o queja con respecto a los Servicios, puede enviarnos un correo electrónico a support@Spootichat.com.."
+    description2: "Si tiene alguna pregunta, reclamo o queja con respecto a los Servicios, puede enviarnos un correo electrónico a support@spootichat.com."
 
 }
