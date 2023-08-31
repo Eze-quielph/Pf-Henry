8ceb28b2dfb0b4a33839636768e3f4c8a20a6e63
Valentina Ortiz <116057704+svalentinaog@users.noreply.github.com>
2023-08-30 19:59:45 -0500

    Delete server directory

--- server/index.js
+++
@@ -1,8 +0,0 @@
-const app = require("./src/app");
-//const { PORT } = require("./src/config");
-const { sequelize } = require("./src/db");
-
-app.listen(3001, () => {
-  sequelize.sync({ force: true });
-  console.log(`Listening on port 3001`);
-});
